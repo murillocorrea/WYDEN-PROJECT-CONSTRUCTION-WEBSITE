377c59a44e1f135944414faacbd5887a0f9ade87
Murillo Correa <murillocorrea0310@gmail.com>
2024-11-26 20:02:18 -0300

    Ajuste da tag por orientacao do validator

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
+   <html lang="en">
    <script>
       const BACKEND_URL = "https://caeb-170-83-72-252.ngrok-free.app";
    </script>
-   <html lang="en">
    <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
